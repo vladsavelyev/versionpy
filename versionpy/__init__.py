47524680daac6085d8dccbde7d1430d2bfb5e61a
vladsaveliev <vladislav.sav@gmail.com>
2019-01-30 17:21:41 +1100

    Add `version` command

--- versionpy/__init__.py
+++ versionpy/__init__.py
@@ -20,22 +20,47 @@ def get_component_ind(component_name):
     return None
 
 
-def get_version(pkg):
+def get_version(pkg, silent=False):
+    """
+    Used by `version` script
+    """
     if not pkg:
-        pkg = _find_package(pkg)
-    return str(_get_cur_version(pkg))
+        pkg = _find_versioned_package(pkg, silent=silent)
+        if not pkg:
+            critical(f'Could not find a package with _version.py. Initialize with `bump` if you want to use versionpy.')
+    return str(_get_cur_version(pkg, silent=silent))
 
 
 def increment_version(arg='patch', pkg=None):
+    """
+    Used by `bump` script
+    """
+    if pkg and not isdir(pkg):
+        log(f'Folder {pkg} does not exist')
+
+    versioned_pkg = _find_versioned_package(pkg)
+    if not versioned_pkg:
+        if not pkg:
+            pkg = _find_folder_to_package()
+            if not pkg:
+                critical('Could not find a package with _version.py, neither a suitable python package to version')
+        log(f'Inititalising {pkg}/_version.py')
+    else:
+        pkg = versioned_pkg
+
+    version_py = join(pkg, '_version.py')
+
     if get_component_ind(arg) is not None:  # one of version component names
-        cur_version = StrictVersion(get_version(pkg))
-        if cur_version is None:
+        if not versioned_pkg:
             new_version = StrictVersion('0.0')
-            err(f'Initialising with version {new_version}')
+            log(f'Initialising with version {new_version}')
         else:
+            cur_version = StrictVersion(_get_cur_version(pkg))
+            if not cur_version:
+                critical(f'Cannot parse version from {version_py}')
             components = list(cur_version.version)
             component_ind = get_component_ind(arg)
-            err(f'Incrementing {arg} component {components[component_ind]}->{components[component_ind] + 1}')
+            log(f'Incrementing {arg} component {components[component_ind]}->{components[component_ind] + 1}')
             components[component_ind] = str(int(components[component_ind]) + 1)
             for lower_component_ind in range(component_ind + 1, len(components)):
                 components[lower_component_ind] = 0
@@ -47,8 +72,6 @@ def increment_version(arg='patch', pkg=None):
     else:
         new_version = StrictVersion(arg)
 
-    pkg = _find_package(pkg)
-    version_py = join(pkg, '_version.py')
     git_rev = get_git_revision()
     with open(version_py, 'w') as f:
         f.write((
@@ -56,7 +79,7 @@ def increment_version(arg='patch', pkg=None):
             f'__version__ = \'{new_version}\'\n' +
             f'__git_revision__ = \'{git_rev}\'') + '\n')
 
-    err(f'New version: {new_version}, written to {version_py}')
+    log(f'New version: {new_version}, written to {version_py}')
     return version_py, new_version
 
 
@@ -108,7 +131,7 @@ def get_git_revision():
     return git_revision
 
 
-def _get_cur_version(pkg):
+def _get_cur_version(pkg, silent=False):
     """ Tries to find _version.py or VERSION.txt with a value for a current version. On failure, returns None.
     """
     version_py = join(pkg, '_version.py')
@@ -118,47 +141,83 @@ def _get_cur_version(pkg):
             for l in f:
                 if l.startswith('__version__ = '):
                     ver_str = l.split(' = ')[1].strip().strip("'")
-                    print(ver_str)
                     break
         assert ver_str, f'Cannot read verion from file {version_py}'
         cur_version = StrictVersion(ver_str)
-        err(f'Current version, read from {version_py}: {cur_version}')
+        log(f'Current version, read from {version_py}: {cur_version}', silent=silent)
     else:
         version_txt = 'VERSION.txt'
         if isfile(version_txt):
             cur_version = StrictVersion(open(version_txt).read().strip())
-            err(f'Current version, read from {version_txt}: {cur_version}')
+            log(f'Current version, read from {version_txt}: {cur_version}', silent=silent)
         else:
             cur_version = None
     return cur_version
 
 
-def _find_package(pkg=None):
+def _find_versioned_package(pkg=None, silent=False):
     found_verpy = glob.glob(f'{pkg or "*"}/_version.py')
     if len(found_verpy) > 1:
-        err(f'Found multiple packages with _version.py: {found_verpy}. Please leave only one, it should be the main '
-            f'package of your tool that you want to version.')
+        log(f'Found multiple packages containing _version.py: {found_verpy}. If you want to version this project, '
+            f'either leave one main package with _verison.py, or specify the target version with -p option')
         sys.exit(1)
     elif len(found_verpy) == 1:
         pkg = dirname(found_verpy[0])
-        err(f'Found package with _version.py file: {pkg}')
+        log(f'Found package with _version.py file: {pkg}', silent=silent)
+        return pkg
     else:
-        pkg = None
-        try:
-            import setuptools
-            pkgs = setuptools.find_packages()
-            if len(pkgs) > 1:
-                pkg = input(f'Multiple packages found: {pkgs}. Please specify the main package to version. '
-                            f'It will create _version.py file in it and use it to keep track of the version:')
-            elif len(pkgs) == 1:
-                pkg = pkgs[0]
-                err(f'Inititalising _version.py in package {pkg}')
-        except:
-            pass
-        if pkg is None:
-            pkg = input(f'Could not find any packages to version. Please, specify folder to inititate _version.py:')
+        return None
+
+
+def _find_folder_to_package():
+    pkg = None
+    try:
+        import setuptools
+        pkgs = setuptools.find_packages()
+        if len(pkgs) > 1:
+            pkg = input(f'Multiple packages found: {pkgs}. Please specify the main package to version. '
+                        f'It will create _version.py file in it and use it to keep track of the version'
+                        f'(alternatively, you can rerun with `-p <package_name>`):')
+            while not isdir(pkg):
+                dirs = [d for d in os.listdir(os.getcwd()) if isdir(d)]
+                pkg = input(f'Folder {pkg} does not exist. Available folders: {", ".join(dirs)}')
+        elif len(pkgs) == 1:
+            pkg = pkgs[0]
+    except:
+        pass
+
+    if not pkg:
+        pkg = input(f'Could not find any packages to version. Please specify folder to initiate _version.py '
+                    f'(alternatively, you can rerun with `-p <package_name>`):')
+        while not isdir(pkg):
+            dirs = [d for d in os.listdir(os.getcwd()) if isdir(d)]
+            pkg = input(f'Folder {pkg} does not exist. Available folders: {", ".join(dirs)}')
+
     return pkg
 
 
-def err(msg=''):
-    sys.stderr.write(msg + '\n')
+
+def log(msg='', silent=False):
+    if not silent:
+        sys.stderr.write(msg + '\n')
+
+
+def critical(msg=''):
+    log(msg)
+    sys.exit(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
